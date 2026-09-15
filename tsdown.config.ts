4803dbf36441d332122049f34b1c77e068fddd81
Bruno Alves <nito.ba.dev@gmail.com>
2026-09-15 12:50:01 -0300

    chore: init

---
+++ tsdown.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+  entry: {
+    index: "src/index.ts",
+  },
+  format: ["esm"],
+  platform: "node",
+  target: "es2023",
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  treeshake: true,
+  deps: {
+    neverBundle: [
+      /^@nestjs\//,
+      "reflect-metadata",
+      "rxjs",
+      "zod",
+      "bun:sqlite",
+    ],
+  },
+});
